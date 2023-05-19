2448a20d0746ea981b0eea729476a0ed245e6b4f
indakwa <indakwabenedict@gmail.com>
2023-05-19 17:31:31 +0300

    added buy button

--- js/products.js
+++ js/products.js
@@ -1,10 +1,34 @@
-const boxes = document.querySelectorAll('.box');
+// const boxes = document.querySelectorAll('.box');
 
-boxes.forEach(box => {
-  box.addEventListener('click', () => {
-    const boxId = box.getAttribute('id');
-    localStorage.setItem('s_prdct', boxId);
-    console.log(boxId)
-    location.href = "details.html"
-  });
-})
+// boxes.forEach(box => {
+//   box.addEventListener('click', () => {
+//     const boxId = box.getAttribute('id');
+//     localStorage.setItem('s_prdct', boxId);
+//     console.log(boxId)
+//     location.href = "details.html"
+//   });
+// })
+
+function decreaseValue(productId) {
+  var input = document.getElementById(productId);
+  var value = parseInt(input.value);
+  if (value > 0) {
+    input.value = value - 1;
+  }
+}
+
+function increaseValue(productId) {
+  var input = document.getElementById(productId);
+  var value = parseInt(input.value);
+  input.value = value + 1;
+}
+
+function purchase(productID, productNo) {
+  var input = document.getElementById(productID);
+  var quantity = parseInt(input.value);
+
+  localStorage.setItem('qtty', quantity);
+  localStorage.setItem('pNo', productNo)
+
+  location.href = "buy.html";
+}
--- js/products2.js
+++ js/products2.js
@@ -1,49 +1,25 @@
 const products = [
     {
         p_name: "Simba Cement 32.5R",
-        list_items: `
-        <li>Faster drying and settling time</li>
-        <li>Greater comprehensive strength</li>
-        <li>Better coverage comparatively</li>
-        <li>UNBS certified</li>
-        <li>Suitable for all general purpose applications</li>
-        <li>No breakage or spillage due to eco-friendly packing</li>
-        <li>High grade of fineness</li>
-        `,
-        price: "KSh. 480.00",
-        src: "bag.png"
+        price: 480
     },
     {
         p_name: "Simba Power 42.5N",
-        list_items: `
-        <li>Suitable for specialized constructions</li>
-        <li>Versatile application for bridges, towers, pavements, highways, high-rise towers and more.</li>
-        <li>High supportive strength to the structures</li>
-        <li>Specific international formulation</li>
-        `,
-        price: "KSh. 570.00",
-        src: "../images/bag2.png"
+        price: 570
     },
     {
         p_name: "Paving Blocks",
-        list_items: `
-        <li>Extremely durable and can withstand harsh weather conditions </li>
-        <li>Aesthetically pleasing - designed in a wide range of shapes, sizes, and colors</li>
-        <li>Relatively easy to install</li>
-        <li>Environmentally friendly </li>
-        `,
-        price: "KSh. 250.00",
-        src: "../images/block.png"
+        price: 250
     },
 ]
 
-const img = document.getElementById('myImg')
-const h3 = document.getElementById('myH3')
-const container = document.querySelector('.container');
-const ul = container.querySelector('ul');
-const product = products[localStorage.getItem('s_prdct')]; 
+const product = document.getElementById('product')
+const price = document.getElementById('price')
+const quantity = document.getElementById('quantity')
+const total = document.getElementById('total');
+const productNo = localStorage.getItem('pNo')
 
-// Add the list items to the <ul> element
-ul.innerHTML = product.list_items;
-img.src = `../images/${product.src}`;
-h3.innerText = product.p_name;
+product.innerText = products[productNo].p_name;
+price.innerText = products[productNo].price;
+quantity.innerText = localStorage.getItem('qtty')
+total.innerText = (localStorage.getItem('qtty') * products[productNo].price).toLocaleString()
