9c36880970c216f25f3b0c5e22ae81872e8a6c46
indakwa <indakwabenedict@gmail.com>
2023-12-01 00:14:57 +0300

    added some more products

--- js/products2.js
+++ js/products2.js
@@ -23,6 +23,42 @@ const products = [
         p_name: "ROUND STEEL BARS",
         price: [190, 695, 1065, 1512, 2705]
     },
+    {
+        p_name: "SQUARE STEEL BARS",
+        price: [870, 1240, 550, 2190]
+    },
+    {
+        p_name: "FLAT STEEL BARS",
+        price: [585, 450, 1120, 1800, 2000, 1155, 920, 920]
+    },
+    {
+        p_name: "SQUARE TUBE STEEL BARS",
+        price: [5250, 9500, 9500, 1040, 730, 1710, 1180, 1320, 920, 2170, 1760, 825, 605]
+    },
+    {
+        p_name: "DEVKI DEFORMED TMT METAL BARS",
+        price: [700, 1070, 1535, 2710, 4340, 6900, 10900]
+    },
+    {
+        p_name: "RECTANGULAR TUBE STEEL BAR",
+        price: [5335, 7000, 6260, 960, 1350, 1580, 1120, 3600]
+    },
+    {
+        p_name: "TEE STEEL BARS",
+        price: [1650, 1050]
+    },
+    {
+        p_name: "U CHANNEL STEEL BARS",
+        price: [6500, 5200]
+    },
+    {
+        p_name: "BLACK STEEL PIPE",
+        price: [1100, 1550, 2100, 2450, 3150, 4000, 6350, 7200]
+    },
+    {
+        p_name: "ANGLE LINE STEEL BARS",
+        price: [4250, 1100, 2920, 2000, 2400, 8000, 2150, 755, 2750]
+    },
 ]
 
 const product = document.getElementById('product')
