c3a69c51e1cc16eeb33e151e45cd416688b2aea4
indakwa <indakwabenedict@gmail.com>
2023-12-12 10:21:00 +0300

    updated paybill ac no

--- js/buy.js
+++ js/buy.js
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function showPaybill(){
-    alert(`Please use these details to pay \n PAYBILL: 329329 \n ACCOUNT NUMBER: 0100438301900`)
+    alert(`Please use these details to pay \n PAYBILL: 329329 \n ACCOUNT NUMBER: 0100442039100`)
 
     //localStorage.clear();
 }
